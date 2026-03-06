244dab3526e2375375826222947f410b9cef30aa
404area <50036307+404area@users.noreply.github.com>
2026-03-06 14:21:57 +0800

    修改订阅地址

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -339,13 +339,13 @@ export default {
         },
         customBackend: {
           "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.404area.vip",
-          "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.404area.vip",
+          "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.169988.xyz",
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
           "肥羊备用后端【vless reality+hy1+hy2】": "https://api.v1.mk",
         },
         backendOptions: [
           { value: "https://subapi.404area.vip" },
-          { value: "https://subapi.404area.vip" },
+          { value: "https://subapi.169988.xyz" },
           { value: "https://url.v1.mk" },
           { value: "https://api.v1.mk" },
         ],
@@ -1304,4 +1304,5 @@ export default {
   }
 };
 </script>
+
 
